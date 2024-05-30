fb1a3a61d736040f6a0f62e7edc9d85d425734a7
MyPC <faisaljulio11.fj@gmail.com>
2024-05-30 07:49:36 +0700

    image banner

--- app/src/main/java/com/gunder/market/MainActivity.kt
+++ app/src/main/java/com/gunder/market/MainActivity.kt
@@ -3,19 +3,22 @@ package com.gunder.market
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -128,13 +131,16 @@ fun MainTopcategoryPreview() {
 
 @Composable
 fun MainImageBanner() {
-    LazyVerticalGrid(GridCells.Fixed(2)){
-        items(dummyListImageBanner){
-            ImageBanner(listImageBanner = it)
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center){
+            ImageBanner(listImageBanner = dummyListImageBanner.take(2))
         }
-
+        Row(Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.Center){
+            ImageBanner(listImageBanner = dummyListImageBanner.takeLast(2))
+        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/gunder/market/component/ImageBanner.kt
+++ app/src/main/java/com/gunder/market/component/ImageBanner.kt
@@ -1,27 +1,45 @@
 package com.gunder.market.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.gunder.market.R
 import com.gunder.market.model.ListImageBanner
 import com.gunder.market.ui.theme.MarketTheme
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ImageBanner(listImageBanner: ListImageBanner) {
-        Row {
-            Image(
-                painter = painterResource(id = listImageBanner.imgBannerHor),
-                contentDescription = null)
+fun ImageBanner(listImageBanner: List<ListImageBanner>) {
+    listImageBanner.forEach  { Banner ->
+        Row (){
+            Card (modifier = Modifier.padding(end = 8.dp)){
+                Image(
+                    painter = painterResource(id = Banner.imgBannerHor),
+                    contentDescription = null,
+
+                )
+            }
         }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ImageBannerPrev() {
     MarketTheme {
-        ImageBanner(listImageBanner = ListImageBanner(R.drawable.banner_horizontal_1))
+        ImageBanner(
+            listImageBanner = listOf(ListImageBanner(R.drawable.banner_horizontal_1),
+                                    ListImageBanner(R.drawable.banner_horizontal_2)
+            )
+        )
     }
 }
